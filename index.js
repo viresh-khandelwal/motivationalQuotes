4260b875aa04262a92b5d8c451c33b44cc312fda
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2023-06-04 18:18:21 +0530

    created fiest endpoint and installed nodemon

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const port = 3000;
 expressServer.listen(port, () => {
     console.log('server started');
 });
+
+expressServer.get('/motivationalQuotes', (req,res) => {
+    console.log('my first route');
+    res.send('my first response');
+})
