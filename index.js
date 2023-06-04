f95d5ec3aeea9696797525ec0a059e5d25848efe
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2023-06-04 18:51:47 +0530

    added code to fetch body from request body

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const quotes = require('./data/quotes.json');
 
 expressServer = express();
+expressServer.use(express.json()); //this is written to make the code able to fetch body from request body
 const port = 3000;
 expressServer.listen(port, () => {
     console.log('server started');
@@ -9,5 +10,6 @@ expressServer.listen(port, () => {
 
 expressServer.get('/motivationalQuotes', (req,res) => {
     console.log('my first route');
+    console.log(req.body);
     res.json(quotes);
 })
