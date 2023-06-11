90ee528851faf19f55ed7e8dd60c0b35b6a302be
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2023-06-11 20:13:35 +0530

    connected to DB

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const quotes = require('./data/quotes.json');
+const mongoose = require('mongoose');
 
 expressServer = express();
 expressServer.use(express.json()); //this is written to make the code able to fetch body from request body
 const port = 3000;
+
+const db_user_name = 'viresh';
+const db_pwd = 'Viresh%4019'; //actual password is Viresh@19 but @ needs to be url encoded to %40
+const DB_URL = `mongodb+srv://${db_user_name}:${db_pwd}@mongo-db-learning.82bygnn.mongodb.net/?retryWrites=true&w=majority`;
+
+//start server
 expressServer.listen(port, () => {
     console.log('server started');
 });
+
+//connect to database
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('db connected');
+})
 
 expressServer.get('/motivationalQuotes', (req,res) => {
     console.log(req.body);
